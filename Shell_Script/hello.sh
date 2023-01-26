1d2b75eefd4ff019eee5690978d3937ec94e96c0
Renan Martins <renanmartins1999@hotmail.com>
2023-01-25 23:02:51 -0300

    Adding hello.sh script

---
+++ Shell_Script/hello.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# This is a comment 
+echo "Hello World" # This is another comment
+
+echo Bash Path: $BASH 
+echo Bash version: $BASH_VERSION
+echo Home directory: $HOME
+echo Current working directory: $PWD
+
+name=Renan
+
+echo My name is $name
