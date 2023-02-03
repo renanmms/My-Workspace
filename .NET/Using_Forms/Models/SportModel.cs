f718325560feb8edc8e900ffe09a04956befef8d
Renan Martins <renanmartins1999@hotmail.com>
2023-02-02 21:48:02 -0300

    Adicionando validações nos campos
    Adicionando validações nos campos de nome e ano

--- .NET/Using_Forms/Models/SportModel.cs
+++ .NET/Using_Forms/Models/SportModel.cs
@@ -1,6 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HTML_Tag_Helper.Models;
 
 public class SportModel {
+    [Required(ErrorMessage = "O nome é obrigatório!")]
     public string Name { get; set; }
-    public int Years { get; set; }
+    
+    [Required(ErrorMessage = "A quantidade de anos é obrigatória!")]
+    public int? Years { get; set; }
 }
