4f12c7a1c1f9acc27f6e3a33156010cd730daa38
Renan Martins <renanmartins1999@hotmail.com>
2023-03-23 23:09:52 -0300

    Exemplo de baixar arquivo pdf

---
+++ .NET/Print_PDF_File/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Print_PDF_File.Models;
+
+namespace Print_PDF_File.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult GerarPDF(int id){
+        return View("Index");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ .NET/Print_PDF_File/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Print_PDF_File.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
