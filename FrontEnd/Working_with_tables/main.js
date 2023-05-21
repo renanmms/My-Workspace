333ef1fb6ec8cebd850088392294f54369edd667
Renan Martins <renanmartins1999@hotmail.com>
2023-05-21 18:56:48 -0300

    Adicionando classe com JQuery

--- FrontEnd/Working_with_tables/main.js
+++ FrontEnd/Working_with_tables/main.js
@@ -1,5 +1,5 @@
 let linhas = $("tr[id^='linha-']");
 
-console.log(`${linhas.length}`);
-
-linhas[0].toggleClass();
+linhas.each(function (index){
+    $("#linha-" + (index + 1)).addClass("peca-nao-aceita");
+});
